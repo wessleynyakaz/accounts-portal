b6af0651a4cf7769dd0fb5e184ed4ff0eb56bef9
Wessley <wessleynyakaz@gmail.com>
2024-04-28 15:35:55 +0200

    (chore) fixed layout problem and renamed .env file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
-import '@/app/ui/global.css'
- 
-export default function Layout({ children }: { children: React.ReactNode }) {
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
-      </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
   );
 }
